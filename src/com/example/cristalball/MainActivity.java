e2ca102e49edf3f7b46e063be01285b53b805228
Robin <robinchogiles@gmail.com>
2012-12-22 18:04:43 +0100

    Declare our view variables TextView

--- src/com/example/cristalball/MainActivity.java
+++ src/com/example/cristalball/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.cristalball;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,11 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // Declare our view variables
+        TextView answerLabel = (TextView) findViewById(R.id.textView1);
+        Button getAnswerButton;
+        
     }
 
     @Override
