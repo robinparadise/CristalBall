4e45ac0f3dc7db4748d8c0a487783c09686534c9
Robin <robinchogiles@gmail.com>
2012-12-22 18:21:43 +0100

    when click the button "YES"

--- src/com/example/cristalball/MainActivity.java
+++ src/com/example/cristalball/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.cristalball;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,8 +15,17 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         // Declare our view variables
-        TextView answerLabel = (TextView) findViewById(R.id.textView1);
+        final TextView answerLabel = (TextView) findViewById(R.id.textView1);
         Button getAnswerButton = (Button) findViewById(R.id.button1);
+        
+        getAnswerButton.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				// The button was clicked, so update the label text to the answer
+				String answer = "Yes";
+				answerLabel.setText(answer);				
+			}
+		});
         
     }
 
