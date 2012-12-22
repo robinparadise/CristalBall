1a6121c64b045c980525a51c2ec4568269fd7387
Robin <robinchogiles@gmail.com>
2012-12-22 18:59:47 +0100

    Convert the randomNumber to a text answer

--- src/com/example/cristalball/MainActivity.java
+++ src/com/example/cristalball/MainActivity.java
@@ -29,7 +29,23 @@ public class MainActivity extends Activity {
 				// Randomly select one of the three answers: YES, NO, MAYBE
 				Random randomGenerator = new Random(); // construct a new Random Number Generator
 				int randomNumber = randomGenerator.nextInt(3);
-				answer = Integer.toString(randomNumber);
+				/* Convert the randomNumber to a text answer
+				 * 0 = Yes
+				 * 1 = No
+				 * 2 = Maybe
+				 */
+				if (randomNumber == 0) {
+					answer = "Yes";
+				}
+				else if (randomNumber == 1) {
+					answer = "No";
+				}
+				else if (randomNumber == 2) {
+					answer = "Maybe";
+				}
+				else {
+					answer = "Sorry there was an error";
+				}
 				
 				// Update the answer with our dinamic answer
 				answerLabel.setText(answer);				
