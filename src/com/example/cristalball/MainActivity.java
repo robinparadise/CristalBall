2421339222f2d83bf88b872742e74a7dab28cb9e
Robin <robinchogiles@gmail.com>
2012-12-24 00:30:00 +0100

    Shaking Things Up

--- src/com/example/cristalball/MainActivity.java
+++ src/com/example/cristalball/MainActivity.java
@@ -2,22 +2,26 @@ package com.example.cristalball;
 
 import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 import android.view.animation.AlphaAnimation;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.cristalball.ShakeDetector.OnShakeListener;
 
 public class MainActivity extends Activity {
 
 	private CrystalBall mCrystalBall = new CrystalBall();
 	private TextView mAnswerLabel;
-	private Button mGetAnswerButton;
 	private ImageView mCrystalBallImage;
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private ShakeDetector mShakedetector;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,26 +30,31 @@ public class MainActivity extends Activity {
         
         // Declare our view variables
         mAnswerLabel = (TextView) findViewById(R.id.textView1);
-        mGetAnswerButton = (Button) findViewById(R.id.button1);
+        mCrystalBallImage = (ImageView) findViewById(R.id.imageView1);
         
-        mGetAnswerButton.setOnClickListener(new View.OnClickListener() {
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakedetector = new ShakeDetector(new OnShakeListener() {
 			
-			public void onClick(View v) {
-				String answer = mCrystalBall.getAnAnswer();
-				
-				// Update the answer with our dinamic answer
-				mAnswerLabel.setText(answer);
-				// Animate the crystal ball
-				animateCrystalBall();
-				animateAnswer();
-				playSound();
+        	public void onShake() {
+				handleNewAnswer();
 			}
 		});
-        
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	mSensorManager.registerListener(mShakedetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+    
+    @Override
+    public void onPause() {
+    	super.onPause();
+    	mSensorManager.unregisterListener(mShakedetector);
     }
     
     private void animateCrystalBall() {
-    	mCrystalBallImage = (ImageView) findViewById(R.id.imageView1);
     	mCrystalBallImage.setImageResource(R.drawable.ball_animation);
     	AnimationDrawable ballAnimation = (AnimationDrawable) mCrystalBallImage.getDrawable();
     	if (ballAnimation.isRunning()) {
@@ -78,4 +87,15 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
+	private void handleNewAnswer() {
+		String answer = mCrystalBall.getAnAnswer();
+		
+		// Update the answer with our dinamic answer
+		mAnswerLabel.setText(answer);
+		// Animate the crystal ball
+		animateCrystalBall();
+		animateAnswer();
+		playSound();
+	}
 }
